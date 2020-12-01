6433fbc2f1fe46c08d2193a504f332428163b22e
pookiepew <glensorbo@gmail.com>
2020-12-01 18:56:39 +0100

    Added image component

--- src/Song/components/ShowList.js
+++ src/Song/components/ShowList.js
@@ -1,4 +1,8 @@
-import Song from './Song';
+import React, { Suspense } from 'react';
+
+import Spinner from '../../shared/UIElements/Spinner';
+
+const Song = React.lazy(() => import('./Song'));
 
 const ShowList = props => {
   console.log('[ShowList] render');
@@ -7,7 +11,9 @@ const ShowList = props => {
     <ul>
       {props.songs.map(song => (
         <li key={song._id}>
-          <Song song={song} />
+          <Suspense fallback={<Spinner />}>
+            <Song song={song} />
+          </Suspense>
         </li>
       ))}
     </ul>
--- src/Song/components/Song.js
+++ src/Song/components/Song.js
@@ -1,11 +1,19 @@
+import React, { Suspense } from 'react';
+
+import Spinner from '../../shared/UIElements/Spinner';
+
+const Image = React.lazy(() => import('../../shared/UIElements/Image'));
+
 const Song = ({ song }) => {
   console.log('[Song] render');
   return (
     <div>
-      <img
-        src={'http://dev.pookie.one/images/small/' + song.imageName}
-        alt={song.title}
-      />
+      <Suspense fallback={<Spinner />}>
+        <Image
+          src={'https://images.miabelle.tv/images/small/' + song.imageName}
+          alt={song.title}
+        />
+      </Suspense>
       <h1>{song.artist}</h1>
       <p>{song.title}</p>
     </div>
