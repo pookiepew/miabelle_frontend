200c77cdf0f5e1ed23f75ac892d3e08b5a36e59e
pookiepew <glensorbo@gmail.com>
2020-12-07 16:53:01 +0100

    Added Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './shared/components/Navigation/Header';
 import Home from './Home/Home';
 import SongList from './Song/pages/SongList';
+import Footer from './shared/components/Footer';
 
 const App = () => {
   console.log('[APP] render');
@@ -24,6 +25,7 @@ const App = () => {
     <Router>
       <Header />
       <main>{routes}</main>
+      <Footer />
     </Router>
   );
 };
---
+++ src/shared/components/Footer.js
@@ -0,0 +1,16 @@
+import './css/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Website by{' '}
+        <a href='http://' target='_blank' rel='noopener noreferrer'>
+          pookiepew
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
