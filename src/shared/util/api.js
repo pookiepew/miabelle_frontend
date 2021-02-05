092205cb778ef34248c7b056881be40c2c831b6e
pookiepew <glensorbo@gmail.com>
2021-02-05 23:21:00 +0100

    ✔ Removed interceptor from axios api instance

--- src/shared/util/api.js
+++ src/shared/util/api.js
@@ -1,24 +1,10 @@
 import axios from 'axios';
 
-import store from '../redux/store';
-
-import { LOGOUT } from '../redux/actions/types';
-
 const api = axios.create({
   baseURL: 'https://api.miabelle.tv/v1',
   headers: {
     'Content-Type': 'application/json'
   }
 });
-
-api.interceptors.response.use(
-  res => res,
-  err => {
-    if (err.response.data.msg === 'Token is not valid') {
-      store.dispatch({ type: LOGOUT });
-    }
-    return Promise.reject(err);
-  }
-);
 
 export default api;
